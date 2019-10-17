b98e469461d15db24cd387169a0d0f24cc869a6f
Armen <gevorgyanweb@gmail.com>
2019-10-17 22:19:02 +0400

    improvements

--- app/src/main/java/com/example/pantanima/ui/customviews/VerticalSliderView.kt
+++ app/src/main/java/com/example/pantanima/ui/customviews/VerticalSliderView.kt
@@ -3,8 +3,6 @@ package com.example.pantanima.ui.customviews
 import android.content.Context
 import android.graphics.Color
 import android.util.AttributeSet
-import android.util.Log
-import android.view.FocusFinder
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.Button
 import android.widget.LinearLayout
@@ -12,27 +10,31 @@ import android.widget.RelativeLayout
 import android.widget.TextView
 import com.example.pantanima.R
 import android.view.MotionEvent
+import android.view.View
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.DrawableCompat
 
 
 class VerticalSliderView : RelativeLayout {
 
+    private var chooserBtnStartMargin = 30.0f
     private var yDelta: Int = 0
     private var variantsTvSize = 30f
     private var chooserBtnSize = 60f
-    private var variantsTvPadding = 20
+    private var variantsTvPadding = 10
     private var variantsTvColor = Color.BLACK
     private var chooserBtnColor = Color.RED
 
     lateinit var variantsContainer: LinearLayout
     lateinit var chooserButton: Button
 
+    var listTv: MutableList<View> = ArrayList()
+
     var listStr: List<String> = ArrayList()
         set(value) {
             field = value
             for (text in field) {
-                drawNewTv(text)
+                listTv.add(drawNewTv(text))
             }
         }
 
@@ -78,6 +80,10 @@ class VerticalSliderView : RelativeLayout {
                     R.styleable.VerticalSliderView_sliderChooserBtnSize,
                     chooserBtnSize
                 )
+                chooserBtnStartMargin = getDimension(
+                    R.styleable.VerticalSliderView_sliderChooserBtnStartMargin,
+                    chooserBtnStartMargin
+                )
             } finally {
                 recycle()
             }
@@ -106,18 +112,20 @@ class VerticalSliderView : RelativeLayout {
         chooserButton.setOnTouchListener(onTouchListener())
         val lp = LayoutParams(chooserBtnSize.toInt(), chooserBtnSize.toInt())
         lp.addRule(END_OF, variantsContainer.id)
+        lp.marginStart = chooserBtnStartMargin.toInt()
         lp.addRule(CENTER_VERTICAL, TRUE)
         chooserButton.layoutParams = lp
         addView(chooserButton)
     }
 
-    private fun drawNewTv(text: String) {
-        val tv = TextView(context)
-        tv.text = text
-        tv.textSize = variantsTvSize
-        tv.setTextColor(variantsTvColor)
-        tv.setPadding(variantsTvPadding, variantsTvPadding, variantsTvPadding, variantsTvPadding)
-        variantsContainer.addView(tv)
+    private fun drawNewTv(str: String): TextView {
+        return TextView(context).apply {
+            text = str
+            textSize = variantsTvSize
+            setTextColor(variantsTvColor)
+            setPadding(variantsTvPadding, variantsTvPadding, variantsTvPadding, variantsTvPadding)
+            variantsContainer.addView(this)
+        }
     }
 
 
@@ -140,7 +148,17 @@ class VerticalSliderView : RelativeLayout {
                     if (newY > top) {
                         if (view.height + newY < bottom) {
                             view.y = newY
-                            getCurrentFocusedPosition(newY)
+
+                            val btnHalfHeight = (view.bottom - view.top) / 2
+                            val binMidCoordinates = newY + btnHalfHeight
+                            val posAndScale = getCurrentFocusedPosition(binMidCoordinates.toInt())
+                            val index = posAndScale.first
+                            val scaleXY = posAndScale.second
+                            if (index != -1) {
+                                val tv = listTv[index]
+                                tv.scaleX = 1 + (scaleXY / 4)
+                                tv.scaleY = 1 + (scaleXY / 4)
+                            }
                         }
                     }
                 }
@@ -150,33 +168,33 @@ class VerticalSliderView : RelativeLayout {
         }
     }
 
-    private fun getCurrentFocusedPosition(rawY: Float): Pair<Int, Float> {
+    private fun getCurrentFocusedPosition(chooserMid: Int): Pair<Int, Float> {
         val totalHeight = variantsContainer.height
         val oneItemHeight = totalHeight / listStr.size
-        for (i in listStr.indices) {
-            val top = height - (height - i * oneItemHeight)
+        for (position in listStr.indices) {
+            val top = height - (height - position * oneItemHeight)
             val bottom = top + oneItemHeight
-            if (top < rawY && bottom > rawY) {
-                val scalePercent = getItemScale(top, bottom, rawY)
-                Log.d("====p", "pos: $i, scalePercent $scalePercent")
-                return Pair(i, 0f)
+            if (chooserMid in (top + 1) until bottom) {
+                val scalePercent = getItemScale(top, bottom, chooserMid)
+                return Pair(position, scalePercent)
             }
         }
         return Pair(-1, 0f) //default
     }
 
-    private fun getItemScale(top: Int, bottom: Int, rawY: Float): Float {
-
+    private fun getItemScale(top: Int, bottom: Int, chooserMid: Int): Float {
         val height = bottom - top
-        val mid = height / 2
-        val diff = Math.max(mid, rawY.toInt()) - Math.min(mid, rawY.toInt())
+        val itemMid = height / 2
 
-        var scalePercent = (diff * 100 / height) - 100
+        val rawMid = bottom - itemMid
+        val diff = if (rawMid > chooserMid) {  //up from mid
+            rawMid - chooserMid
+        } else {  //bottom from mid
+            chooserMid - rawMid
+        }
 
-//        if (rawY > mid) {
-//            scalePercent = 1 - scalePercent
-//        }
-
-        return scalePercent.toFloat()
+        val scalePercent = itemMid - diff
+        val result = scalePercent * 100.0f / itemMid
+        return result * 0.01f
     }
 }
